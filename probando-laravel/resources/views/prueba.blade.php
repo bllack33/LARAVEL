1af79983aeafef61f445f203e8de4682ba9b733f
JuanDa P <bllack33@gmail.com>
2019-01-11 17:04:17 -0500

    laravel

---
+++ probando-laravel/resources/views/prueba.blade.php
@@ -0,0 +1,5 @@
+<h1> pagina de prueba </h1>
+
+<p>el nombre: <strong>{{$nombre}}</strong> viene en un arreglo y por get</p> 
+
+<p>se mandan los datos por el metodo with</p>
--- probando-laravel/routes/web.php
+++ probando-laravel/routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/holamundo',function(){
+	return "hola nuevo proyecto";
+});
+
+Route::match(['get','post'],'/metodos', function(){
+	return '<h3>get y post</h3>';
+});
+
+Route::any('/cualquiera', function(){
+	return '<h3>CUALQUIER METODO CON ANY</h3>';
+});
+
+// el signo de interrogacion se usa para decir que el parametro se puede o no enviar
+// la sentencia WHERE se usa para validad el tipo de dato con expresiones regulares
+// se pasan los datos con un array
+Route::get('/contacto/{nombre?}/{edad?}', function($nombre = "nombre por defecto, si no se manda por url", $edad = '46' ){
+	return view('contacto.contacto', array(
+		"nombre" => $nombre,
+		"edad" => $edad
+	));
+})->where([
+	'nombre'=> '[A-Za-z]+', //solo letras
+	'edad'=> '[0-9]+' //solo numeros
+]);
+
+//se pasan los datos con el metodo WITH
+Route::get('/prueba/{nombre?}/{edad?}', function($nombre = "nombre por defecto, si no se manda por url", $edad = '46' ){
+	return view('prueba')
+			->with('nombre', $nombre)
+			->with('edad',$edad);
+})->where([
+	'nombre'=> '[A-Za-z]+', //solo letras
+	'edad'=> '[0-9]+' //solo numeros
+]);
